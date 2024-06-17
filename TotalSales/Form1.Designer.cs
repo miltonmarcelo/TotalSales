9edd3ebfe55788fd13204829200e9f9b69e37585
Milton Marcelo <30089224+miltonmarcelo@users.noreply.github.com>
2024-06-18 00:42:38 +0100

    Add project files.

---
+++ TotalSales/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TotalSales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(65, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(175, 277);
+            this.listBox1.TabIndex = 0;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.outputLabel.Location = new System.Drawing.Point(307, 106);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(310, 88);
+            this.outputLabel.TabIndex = 1;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(65, 368);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(209, 367);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Total Sales";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ TotalSales/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TotalSales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int[] sales = new int[0]; //Creating the array to store the values of the sales from the file, initialized as empty
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //First Pass: couting the number of lines in the file
+                //Initializing variable to 0 to keep track of the lines
+
+                int lineCount = 0;
+
+                using (StreamReader countFile = File.OpenText(@"C:\Users\milto\OneDrive\Desktop\DBS\ProgramsClass\sales.txt"))
+                {
+                    while (countFile.ReadLine() != null) //Keep couting single lines until it reaches the end of the file, when it will return null
+                    {
+                        lineCount++; //Iterating the lineCount variable
+                    }
+
+                }
+
+                sales = new int[lineCount];
+                int total = 0;
+
+
+                using (StreamReader inputfile = File.OpenText(@"C:\Users\milto\OneDrive\Desktop\DBS\ProgramsClass\sales.txt"))
+                {
+                    for (int index = 0; index <lineCount; index++) //Loop will run the same times as the array size
+                    {
+                        sales[index] = int.Parse(inputfile.ReadLine()); //Reading a single line of text, parsing into integer and storing in the array
+                        total += sales[index];
+                    }
+                   
+
+                }
+
+                listBox1.Items.Clear();
+                foreach (int value in sales)
+                {
+                    listBox1.Items.Add(value);                    
+                }
+
+                outputLabel.Text = "Total sales: " + total.ToString("C2");
+                
+            }
+
+
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }            
+           
+        }
+    }
+}
